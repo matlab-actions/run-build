0ad0c1b26247af64bc2493c1da67c0493be154a2
sameagen <sameagen@mathworks.com>
2023-06-20 10:23:43 -0400

    Add initial startup-options support

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ async function run() {
     };
 
     const command = buildtool.generateCommand(options);
+    const startupOptions = core.getInput("startup-options").split(" ");
 
     const helperScript = await core.group("Generate script", async () => {
         const helperScript = await matlab.generateScript(workspaceDir, command);
@@ -26,7 +27,7 @@ async function run() {
     });
 
     await core.group("Run command", async () => {
-        await matlab.runCommand(helperScript, platform, architecture, exec.exec);
+        await matlab.runCommand(helperScript, platform, architecture, exec.exec, startupOptions);
     });
 }
 
