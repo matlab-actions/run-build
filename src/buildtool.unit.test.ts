75ab617f42a5c26a8fc896b310e4e7dfd02f5994
Kapil Gupta <kapilg@mathworks.com>
2024-01-23 12:04:39 +0530

    Added build options

--- src/buildtool.unit.test.ts
+++ src/buildtool.unit.test.ts
@@ -1,11 +1,12 @@
-// Copyright 2022 The MathWorks, Inc.
+// Copyright 2022-2024 The MathWorks, Inc.
 
 import * as buildtool from "./buildtool";
 
 describe("command generation", () => {
-    it("buildtool invocation with unspecified options", () => {
+    it("buildtool invocation with unspecified tasks and build options", () => {
         const options: buildtool.RunBuildOptions = {
             Tasks: "",
+            AdditionalBuildOptions: "",
         };
 
         const actual = buildtool.generateCommand(options);
@@ -15,10 +16,30 @@ describe("command generation", () => {
     it("buildtool invocation with tasks specified", () => {
         const options: buildtool.RunBuildOptions = {
             Tasks: "compile test",
+            AdditionalBuildOptions: "",
         };
 
         const actual = buildtool.generateCommand(options);
         expect(actual).toBe("buildtool compile test")
+    });
 
+    it("buildtool invocation with only build options", () => {
+        const options: buildtool.RunBuildOptions = {
+            Tasks: "",
+            AdditionalBuildOptions: "-continueOnFailure -skip check",
+        };
+
+        const actual = buildtool.generateCommand(options);
+        expect(actual).toBe("buildtool -continueOnFailure -skip check -ignoreUnknownOptions")
+    });
+
+    it("buildtool invocation with specified tasks and build options", () => {
+        const options: buildtool.RunBuildOptions = {
+            Tasks: "compile test",
+            AdditionalBuildOptions: "-continueOnFailure -skip check",
+        };
+
+        const actual = buildtool.generateCommand(options);
+        expect(actual).toBe("buildtool compile test -continueOnFailure -skip check -ignoreUnknownOptions")
     });
 });
