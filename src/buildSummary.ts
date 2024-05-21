11cfdcb2eb676a41db991b077c994eda3f507636
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-05-21 15:58:23 +0530

    Testing JSON parse

--- src/buildSummary.ts
+++ src/buildSummary.ts
@@ -6,7 +6,7 @@ import { promisify } from 'util';
 // Promisify the readFile function to use it with async/await
 const readFileAsync = promisify(readFile);
 
-interface TaskDetails {
+interface Task {
   name: string;
   description: string;
   failed: boolean;
@@ -14,7 +14,11 @@ interface TaskDetails {
   duration: string;
 }
 
-export async function readJsonFile(filePath: string): Promise<TaskDetails[]> {
+interface TaskList {
+  taskDetails: Task[];
+}
+
+export async function readJsonFile(filePath: string): Promise<TaskList[]> {
   try {
     const data = await readFileAsync(filePath, { encoding: 'utf8' });
     return JSON.parse(data);
@@ -24,20 +28,23 @@ export async function readJsonFile(filePath: string): Promise<TaskDetails[]> {
   }
 }
 
-export function addBuildSummaryTable(task: TaskDetails[]): void {
-console.log("Task Length is" + task.length)
-if (task.length > 0) {
-        task.forEach((row) => {
-          Object.values(row).forEach((value) => {
-            console.log("MY CONSOLE output");
-          });
-        });
-}
+export function addBuildSummaryTable(task: TaskList[]): void {
+console.log("Task Details:");
+tasksData.taskDetails.forEach((task, index) => {
+  console.log(`Task ${index + 1}:`);
+  console.log(`Name: ${task.name}`);
+  console.log(`Description: ${task.description}`);
+  console.log(`Failed: ${task.failed}`);
+  console.log(`Skipped: ${task.skipped}`);
+  console.log(`Duration: ${task.duration}`);
+  console.log('---');
+});
   let fruits: string[] = ["Apple", "Banana", "Orange", "banana"];
   core.summary
   .addHeading('MATLAB Build Results')
   .addTable([
     [{data: 'Task Name', header: true}, {data: 'Status', header: true}, {data: 'Description', header: true}, {data: 'Duration (HH:MM:SS)', header: true}],
+    fruits,
     fruits
   ])
   .write()
