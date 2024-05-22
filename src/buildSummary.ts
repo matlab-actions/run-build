e99eccdcb33a1893a9eb806b7f1b7960340ecf05
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-05-22 16:07:45 +0530

    Refactored mathods for good readability

--- src/buildSummary.ts
+++ src/buildSummary.ts
@@ -69,7 +69,7 @@ export async function processAndDisplayBuildSummary() {
 
   try {
     const runnerTemp = process.env.RUNNER_TEMP;
-    const filePath: string = join(runnerTemp, `buildSummary_${runId}.json`);
+    const filePath = join(runnerTemp as string, `buildSummary_${runId as string}.json`);
     const data = await readJsonFile(filePath);
     const taskSummaryTableRows = getBuildSummaryTable(data);
     writeSummary(taskSummaryTableRows);
