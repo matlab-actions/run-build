118fcd8a0af1cb00368cc342ff10a306178cfb18
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-05-24 12:51:04 +0530

    Added tests

--- src/buildSummary.unit.test.ts
+++ src/buildSummary.unit.test.ts
@@ -6,7 +6,7 @@ import { readFile } from 'fs';
 
 jest.mock('fs', () => ({
  readFile: jest.fn((path: string, options: { encoding: string; flag?: string }, callback: (err: NodeJS.ErrnoException | null, data: string) => void) => {
-}));
+})));
 
 jest.mock('@actions/core', () => ({
   summary: {
