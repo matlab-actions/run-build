514852ffe28e817e65054ddbff1afa9e95d31e36
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-05-24 12:30:55 +0530

    Added tests

---
+++ src/buildSummary.unit.test.ts
@@ -0,0 +1,79 @@
+// Copyright 2024 The MathWorks, Inc.
+
+import * as buildSummary from './buildSummary';
+import * as core from '@actions/core';
+import { readFile } from 'fs';
+
+jest.mock('fs', () => ({
+  readFile: jest.fn(),
+}));
+
+jest.mock('@actions/core', () => ({
+  summary: {
+    addHeading: jest.fn().mockReturnThis(),
+    addTable: jest.fn().mockReturnThis(),
+    write: jest.fn().mockReturnThis(),
+  },
+}));
+
+const mockReadFile = readFile as jest.MockedFunction<typeof readFile>;
+
+describe('readJsonFile', () => {
+  it('reads and parses a JSON file successfully', async () => {
+    const mockData = JSON.stringify({
+      taskDetails: [
+        { name: 'Test Task', description: 'A test task', failed: false, skipped: false, duration: '00:00:10' },
+      ],
+    });
+    mockReadFile.mockImplementation((path, options, callback) => {
+      callback(null, mockData);
+    });
+
+    const filePath = 'path/to/mockFile.json';
+    const data = await buildSummary.readJsonFile(filePath);
+
+    expect(data).toEqual(JSON.parse(mockData));
+    expect(mockReadFile).toHaveBeenCalledWith(filePath, { encoding: 'utf8' }, expect.any(Function));
+  });
+
+  it('throws an error if the file cannot be read', async () => {
+    mockReadFile.mockImplementation((path, options, callback) => {
+      callback(new Error('File not found'), null);
+    });
+
+    const filePath = 'path/to/nonExistentFile.json';
+    await expect(buildSummary.readJsonFile(filePath)).rejects.toThrow('File not found');
+  });
+
+  it('generates a summary table correctly', () => {
+      const mockTasks = {
+        taskDetails: [
+          { name: 'Test Task', description: 'A test task', failed: true, skipped: false, duration: '00:00:10' },
+        ],
+      };
+
+      const expectedTable = [
+        ['Task Name', 'Status', 'Description', 'Duration (HH:MM:SS)'],
+        ['Test Task', '🔴 FAILED', 'A test task', '00:00:10'],
+      ];
+
+      const table = buildSummary.getBuildSummaryTable(mockTasks);
+
+      expect(table).toEqual(expectedTable);
+  });
+
+  it('writes the summary correctly', () => {
+      const mockTableRows = [
+        ['Task Name', 'Status', 'Description', 'Duration (HH:MM:SS)'],
+        ['Test Task', '🔴 FAILED', 'A test task', '00:00:10'],
+      ];
+
+      buildSummary.writeSummary(mockTableRows);
+
+      expect(core.summary.addHeading).toHaveBeenCalledTimes(1);
+      expect(core.summary.addHeading).toHaveBeenCalledWith('MATLAB Build Results');
+
+      expect(core.summary.addTable).toHaveBeenCalledTimes(1);
+      expect(core.summary.addTable).toHaveBeenCalledWith(mockTableRows);
+  });
+});
