c0a4f7c9c5728bb08fa1cc4e699990248f33a634
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-05-24 13:33:49 +0530

    Added tests for json read

--- src/buildSummary.unit.test.ts
+++ src/buildSummary.unit.test.ts
@@ -1,9 +1,9 @@
 // Copyright 2024 The MathWorks, Inc.
 
 import * as buildSummary from './buildSummary';
+import * as fs from 'fs/promises';
 import * as core from '@actions/core';
-import { readFile } from 'fs';
-import * as fs from 'fs';
+
 
 jest.mock('@actions/core', () => ({
   summary: {
@@ -12,8 +12,6 @@ jest.mock('@actions/core', () => ({
     write: jest.fn().mockReturnThis(),
   },
 }));
-
-const mockReadFile = readFile as jest.MockedFunction<typeof readFile>;
 
 describe('summaryGeneration', () => {
   it('generates a summary table correctly', () => {
@@ -46,5 +44,53 @@ describe('summaryGeneration', () => {
 
       expect(core.summary.addTable).toHaveBeenCalledTimes(1);
       expect(core.summary.addTable).toHaveBeenCalledWith(mockTableRows);
+  });
+});
+
+jest.mock('fs/promises');
+
+describe('readJsonFile', () => {
+  const mockReadFileAsync = fs.readFile as jest.MockedFunction<typeof fs.readFile>;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('reads and parses JSON file successfully', async () => {
+    const mockData: TaskList = {
+      taskDetails: [
+        {
+          name: 'Task 1',
+          description: 'Description 1',
+          failed: false,
+          skipped: false,
+          duration: '00:00:10',
+        },
+      ],
+    };
+
+    mockReadFileAsync.mockResolvedValue(JSON.stringify(mockData));
+
+    const filePath = 'path/to/mockFile.json';
+    const result = await buildSummary.readJsonFile(filePath as string);
+
+    expect(mockReadFileAsync).toHaveBeenCalledWith(filePath, { encoding: 'utf8' });
+    expect(result).toEqual(mockData);
+  });
+
+  it('throws an error if the file cannot be read', async () => {
+    mockReadFileAsync.mockRejectedValue(new Error('File not found'));
+
+    const filePath = 'path/to/nonExistentFile.json';
+
+    await expect(buildSummary.readJsonFile(filePath as string)).rejects.toThrow('File not found');
+  });
+
+  it('throws an error if the file contains invalid JSON', async () => {
+    mockReadFileAsync.mockResolvedValue('invalid JSON');
+
+    const filePath = 'path/to/invalidJsonFile.json';
+
+    await expect(buildSummary.readJsonFile(filePath as string)).rejects.toThrow(SyntaxError);
   });
 });
