b35a96cfffdb222d3997b1e2988d32885c446bff
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-07-02 10:44:39 +0530

    Handdled errors close to the functions

--- src/buildSummary.ts
+++ src/buildSummary.ts
@@ -50,15 +50,10 @@ export function processAndDisplayBuildSummary() {
     const runId = process.env.GITHUB_RUN_ID || '';
     const runnerTemp = process.env.RUNNER_TEMP || '';
 
-    let taskSummaryTableRows;
-    try {
-        const filePath: string = join(runnerTemp, `buildSummary${runId}.json`);
-        const data = JSON.parse(readFileSync(filePath, { encoding: 'utf8' }));
-        taskSummaryTableRows = getBuildSummaryTable(data);
-    } catch (e) {
-        console.error('An error occurred while reading the build summary file:', e);
-        return;
-    }
+    const filePath: string = join(runnerTemp, `buildSummary${runId}.json`);
+    const data = JSON.parse(readFileSync(filePath, { encoding: 'utf8' }));
+    const taskSummaryTableRows = getBuildSummaryTable(data);
+
     writeSummary(taskSummaryTableRows);
 }
 
