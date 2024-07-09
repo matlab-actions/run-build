8b40c1634e4408e1543493fca4e6f77520d4d8b2
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-07-09 13:15:02 +0530

    Fixed issue #36

--- src/buildSummary.ts
+++ src/buildSummary.ts
@@ -1,7 +1,7 @@
 // Copyright 2024 The MathWorks, Inc.
 import * as core from "@actions/core";
 import { join } from 'path';
-import { readFileSync } from 'fs';
+import { readFileSync, unlinkSync} from 'fs';
 
 export interface Task {
     name: string;
@@ -41,14 +41,20 @@ export function processAndDisplayBuildSummary() {
     const runId = process.env.GITHUB_RUN_ID || '';
     const runnerTemp = process.env.RUNNER_TEMP || '';
 
+    const filePath: string = join(runnerTemp, `buildSummary${runId}.json`);
     let taskSummaryTableRows;
     try {
-        const filePath: string = join(runnerTemp, `buildSummary${runId}.json`);
         const data = JSON.parse(readFileSync(filePath, { encoding: 'utf8' }));
         taskSummaryTableRows = getBuildSummaryTable(data);
     } catch (e) {
         console.error('An error occurred while reading the build summary file:', e);
         return;
+    } finally {
+        try {
+            unlinkSync(filePath);
+        } catch (e) {
+            console.error(`An error occurred while trying to delete the file ${filePath}:`, e);
+        }
     }
     writeSummary(taskSummaryTableRows);
 }
