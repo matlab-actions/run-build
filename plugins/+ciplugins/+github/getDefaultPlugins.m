896de7e7901da2de3071f714f30b78071eae71b1
Sam Eagen <sameagen@vdi-ah2ddp-153.dhcp.mathworks.com>
2025-04-15 16:40:19 -0400

    Add parallel-safe plugin

--- plugins/+ciplugins/+github/getDefaultPlugins.m
+++ plugins/+ciplugins/+github/getDefaultPlugins.m
@@ -1,14 +1,20 @@
 function plugins = getDefaultPlugins(pluginProviderData)
 
-%   Copyright 2024 The MathWorks, Inc.
+%   Copyright 2024-2025 The MathWorks, Inc.
 
 arguments
     pluginProviderData (1,1) struct = struct();
 end
 
+if isMATLABReleaseOlderThan("R2025b")
+    reportPlugin = ciplugins.github.BuildSummaryPlugin();
+else
+    reportPlugin = ciplugins.github.ParallelizableBuildSummaryPlugin();
+end
+
 plugins = [ ...
     matlab.buildtool.internal.getFactoryDefaultPlugins(pluginProviderData) ...
     ciplugins.github.GitHubLogPlugin() ...
-    ciplugins.github.BuildSummaryPlugin() ...
+    reportPlugin ...
 ];
 end
