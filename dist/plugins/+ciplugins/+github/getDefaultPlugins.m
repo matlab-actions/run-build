2b88a37a20ebd8bfffc799dcc39799cb5fa17249
Sam Eagen <sameagen@vdi-ah2ddp-153.dhcp.mathworks.com>
2025-04-15 16:42:10 -0400

    Add dist folder

---
+++ dist/plugins/+ciplugins/+github/getDefaultPlugins.m
@@ -0,0 +1,20 @@
+function plugins = getDefaultPlugins(pluginProviderData)
+
+%   Copyright 2024-2025 The MathWorks, Inc.
+
+arguments
+    pluginProviderData (1,1) struct = struct();
+end
+
+if isMATLABReleaseOlderThan("R2025b")
+    reportPlugin = ciplugins.github.BuildSummaryPlugin();
+else
+    reportPlugin = ciplugins.github.ParallelizableBuildSummaryPlugin();
+end
+
+plugins = [ ...
+    matlab.buildtool.internal.getFactoryDefaultPlugins(pluginProviderData) ...
+    ciplugins.github.GitHubLogPlugin() ...
+    reportPlugin ...
+];
+end
