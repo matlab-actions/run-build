5969c3e62067e2cf170225bb155fca5e0a50808c
Release Action <>
2025-08-19 21:28:41 +0000

    Build for 8448c17

---
+++ lib/buildSummary.d.ts
@@ -0,0 +1,4 @@
+export declare function writeSummary(taskSummaryTableRows: string[][]): void;
+export declare function getSummaryRows(buildSummary: string): any[];
+export declare function interpretSkipReason(skipReason: string): string;
+export declare function processAndDisplayBuildSummary(): void;
